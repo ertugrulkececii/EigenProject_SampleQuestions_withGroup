85548c08e8187bacf60af77f0a98cf776c96ef8f
ertugrulkececii <ertgrlkcc@gmail.com>
2023-08-28 13:09:38 +0200

    hafta 2

---
+++ src/day01_grupClaismasi/ornek18.java
@@ -0,0 +1,16 @@
+package day01_grupClaismasi;
+
+public class ornek18 {
+    /*18----
+    Girilen Stringdeki tum sesli harfleri saymak icin bir Java Methodu yaziniz.
+
+    Test Data:
+    java is fun
+
+    Beklenen :
+
+    Stringdeki sesli harf sayisi: 4
+    */
+
+
+}
---
+++ src/day01_grupClaismasi/ornek9.java
@@ -0,0 +1,16 @@
+package day01_grupClaismasi;
+
+import java.util.Scanner;
+
+public class ornek9 {
+    public static void main(String[] args) {
+
+
+    /*9----
+    2 farkli String'i  ucuca ekleyen Java Kodunu yaziniz.
+
+     */
+
+
+    }
+}
