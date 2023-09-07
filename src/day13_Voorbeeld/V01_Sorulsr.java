f8ea3a541779bc52345d3badfec1adcef0be662f
ertugrulkececii <ertgrlkcc@gmail.com>
2023-09-08 00:11:30 +0200

    corrigerende

--- src/day13_Voorbeeld/V01_Sorulsr.java
+++ src/day13_Voorbeeld/V01_Sorulsr.java
@@ -19,12 +19,12 @@ public class V01_Sorulsr {
         String password = scanner.next();
 
       if (!(password.charAt(0)>='a' && password.charAt(0)<='z') ){
-          System.out.println("Uw eerst letter van password is niet smal!");
-      } else if (!(password.charAt((password.length()-1))<=9)) {
+          System.out.println("Uw eerst letter van password is niet klein!");
+      } else if (!(password.charAt(password.length()-1))) {
           System.out.println("Uw laatste character is niet cijfer!");
       } else if (!(password.contains(" "))) {
           System.out.println("Uw password heeft gat/gaten!");
-      } else if (!(password.length()==10)) {
+      } else if (!(password.length()>=10)) {
           System.out.println("Uw password lang niet gelijk met 10!");
       } else System.out.println("Password heeft succeesful geregistereerd");
 
